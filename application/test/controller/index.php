13748b918fad3133cce7f095b39ffad1d821fd78
NeverGiveUp <xum1281517532@163.com>
2018-11-20 20:51:57 +0800

    Study thinkphp5.1
    Study thinkphp5.1

--- application/test/controller/index.php
+++ application/test/controller/index.php
@@ -2,11 +2,15 @@
 namespace app\test\controller;
 
 use think\facade\Env;
+use think\Controller;
+use think\facade\Request;
+#use app\test\model\NbaTeam;
 
-class Index
+class Index extends Controller
 {
     public function hello($name = 'ThinkPHP5')
     {
+        echo request()->param('name');
     	$test = new \myExtend\Test();
     	dump($test->sayHello(), true, "test->Index");
     	dump($test->sayGood());
@@ -15,10 +19,29 @@ class Index
     			"think app: ".Env::get("THINK_PATH")."<br />".
     			"config path: ".Env::get("CONFIG_PATH")."<br />".
     			"extend path: ".Env::get("EXTEND_PATH")."<br />".
-    			"vendor path: ".Env::get("VENDOR_PATH")."<br />".
-    			"runtime path: ".Env::get("RUNTIME_PATH")."<br />".
+    			"vendor ptath: ".Env::get("VENDOR_PATH")."<br />".
+    			"runtime pah: ".Env::get("RUNTIME_PATH")."<br />".
     			"route path: ".Env::get("ROUTE_PATH")."<br />".
     			"module path: ".Env::get("MODULE_PATH")."<br />";
         return $html;
+    }
+
+    public function serverinfo() {
+        $html = "url(): ".Request::url()."<br />".
+                "url(true): ".Request::url(true)."<br />".
+                "baseFile(): ".Request::baseFile()."<br />".
+                "baseFile(true): ".Request::baseFile(true)."<br />".
+                "root(): ".Request::root()."<br />".
+                "root(true): ".Request::root(true)."<br />";
+        return $html;
+    } 
+
+    public function gotoSuccess()
+    {
+        $this->success('got to success', 'index/hello');
+    }
+
+    public function gotoError(){
+        $this->error('error');
     }
 }
